521df47929378cdae0a39002e11f0ca2a3408c3b
cjlotto <joung6517@gmail.com>
2023-06-19 23:35:47 +0900

    main setup & user router

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import Depends, FastAPI
+from starlette.middleware.cors import CORSMiddleware
+from starlette import status
+from user.router import router as user_router
+
+app = FastAPI()
+
+origins = [""]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(user_router)
+
+
+@app.get("/health_check", status_code=status.HTTP_200_OK)
+def health_check():
+    return {"health_check": True}
