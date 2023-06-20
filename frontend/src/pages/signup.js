860f991f8366452e35188500a973e57823ac0659
cjlotto <joung6517@gmail.com>
2023-06-21 00:13:04 +0900

    signup setup UI

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,28 +1,33 @@
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 function SignUp() {
     return (
-        <Form>
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-                <Form.Label column sm="2">
-                    Email
-                </Form.Label>
-                <Col sm="10">
-                    <Form.Control type="email" defaultValue="email@example.com" />
-                </Col>
-            </Form.Group>
+        <div>
+            <Form>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="email" defaultValue="email@example.com" />
+                    </Col>
+                </Form.Group>
 
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-                <Form.Label column sm="2">
-                    Password
-                </Form.Label>
-                <Col sm="10">
-                    <Form.Control type="password" placeholder="Password" />
-                </Col>
-            </Form.Group>
-        </Form>
+                <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Col>
+                </Form.Group>
+            </Form>
+            <Button variant="success">Sign In</Button>{' '}
+            <Button variant="primary">Sign Up</Button>{' '}
+        </div>
     );
 }
 
