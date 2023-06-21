0eae5fab236f493883003f28424880a96a9e6c13
cjlotto <joung6517@gmail.com>
2023-06-22 00:29:55 +0900

    구문 수정

--- frontend/src/pages/signin/signin.js
+++ frontend/src/pages/signin/signin.js
@@ -14,8 +14,8 @@ function SignIn() {
 
                                 <div className="mb-md-5 mt-md-4 pb-5">
 
-                                    <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                                    <p className="text-white-50 mb-5">Please enter your login and password!</p>
+                                    <h2 className="fw-bold mb-2 text-uppercase">Sign In</h2>
+                                    <p className="text-white-50 mb-5">Please enter your email and password!</p>
 
                                     <div className="form-outline form-white mb-4">
                                         <input type="email" id="typeEmailX" className="form-control form-control-lg" />
@@ -29,7 +29,7 @@ function SignIn() {
 
                                     <p className="small mb-5 pb-lg-2"><a className="text-white-50" href="#!">Forgot password?</a></p>
 
-                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                                    <button className="btn btn-outline-light btn-lg px-5" type="submit">Sign In</button>
 
                                     <div className="d-flex justify-content-center text-center mt-4 pt-1">
                                         <a href="#!" className="text-white"><i className="fab fa-facebook-f fa-lg"></i></a>
@@ -37,8 +37,9 @@ function SignIn() {
                                         <a href="#!" className="text-white"><i className="fab fa-google fa-lg"></i></a>
                                     </div>
                                 </div>
+
                                 <div>
-                                    <p className="mb-0">Don't have an account?
+                                    <p className="mb-0">Don't you have an account?
                                         <div onClick={() => { navigate("/sign-up") }}>
                                             <p className="text-white-50 fw-bold pointer">Sign Up</p>
                                         </div>
