aadacf5c82c0e034b8780bf8461d60d082ebd1ed
cjlotto <joung6517@gmail.com>
2023-06-21 23:59:20 +0900

    SignIn, SignUp setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/home/home.js'
+import { Routes, Route } from 'react-router-dom'
+import SignIn from './pages/signin/signin.js'
+import SignUp from './pages/signup/signup.js';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={< SignUp />}></Route>
+      </Routes>
     </div>
   );
 }
