bc0398bff597c937dcef2819b0580b6a1d58011a
cjlotto <joung6517@gmail.com>
2023-06-25 18:23:41 +0900

    주석 삭제

--- practice/jwt.py
+++ practice/jwt.py
@@ -4,10 +4,6 @@ import hashlib
 import base64
 import json
 
-"""
-Reference : https://webisfree.com/2020-11-07/python-base64-%EC%9D%B8%EC%BD%94%EB%94%A9-%EB%94%94%EC%BD%94%EB%94%A9-%EB%B3%80%ED%99%98-%EB%B0%A9%EB%B2%95
-"""
-
 
 def convert_to_base64(data: Union[str, dict]) -> str:
     # Python에서 base64로 인코딩하려면 str type을 bytes type으로 인코딩, 디코딩하는 과정이 필요하다.
