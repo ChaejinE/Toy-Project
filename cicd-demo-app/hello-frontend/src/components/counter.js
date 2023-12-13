a2d1a31d5cb524604a10c9c88862dd7f7ad9c3f7
ChaejinE <joung6517@gmail.com>
2023-12-13 13:28:38 +0900

    변경 테스트

--- cicd-demo-app/hello-frontend/src/components/counter.js
+++ cicd-demo-app/hello-frontend/src/components/counter.js
@@ -15,6 +15,7 @@ export default function Counter() {
         <div>
             <h2>Counter</h2>
             <p>{count}</p>
+            <p>리진선님입니다</p>
             <button onClick={handleIncrement}>+</button>
             <button onClick={handleDecrement}>-</button>
         </div>
